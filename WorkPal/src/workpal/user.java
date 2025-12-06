35e05c6f9800695b892a9d922b6f3a7b99719211
saja <sajabenmussa@gmail.com>
2025-12-06 23:18:32 +0200

    main class

--- WorkPal/src/workpal/user.java
+++ WorkPal/src/workpal/user.java
@@ -1,10 +1,7 @@
 
 package workpal;
 
-/**
- *
- * @author enasp
- */
+
 public class user {
    
     private int id;
@@ -19,14 +16,26 @@ public class user {
     }
 
     // Getters & Setters
-    public String getUsername() { return username; }
-    public void setUsername(String username) { this.username = username; }
+    public String getUsername() { 
+        return username; 
+    }
+    public void setUsername(String username) { 
+        this.username = username;
+    }
 
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
+    public String getPassword() { 
+        return password; 
+    }
+    public void setPassword(String password) {
+        this.password = password; 
+    }
 
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+    public String getEmail() { 
+        return email; 
+    }
+    public void setEmail(String email) {
+        this.email = email; 
+    }
 }
 
 
