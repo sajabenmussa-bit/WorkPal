fa6376cb345c6b933218431aaf1361a283d302c0
saja <sajabenmussa@gmail.com>
2025-12-06 23:05:41 +0200

    data base connection

---
+++ WorkPal/src/workpal/DBConnection.java
@@ -0,0 +1,20 @@
+package workpal;
+
+import java.sql.*;
+
+public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/workpal"; 
+    private static final String USER = "root"; 
+    private static final String PASSWORD = ""; 
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
+
+    
