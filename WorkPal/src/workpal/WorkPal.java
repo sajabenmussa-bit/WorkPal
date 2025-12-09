31d3339d495136d50d9dabf3c5e9b6cf01998da4
sajabenmussa-bit <sajabenmussa@gmail.com>
2025-12-09 20:42:00 +0200

    main class

--- WorkPal/src/workpal/WorkPal.java
+++ WorkPal/src/workpal/WorkPal.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 package workpal;
 
@@ -14,25 +13,5 @@ public class WorkPal {
             }
         });
     }
-=======
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package workpal;
-
-/**
- *
- * @author Bashaer
- */
-public class WorkPal {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
->>>>>>> my-feature
 }
+
