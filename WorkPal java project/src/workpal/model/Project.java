59324364bc26a50ee96a8fb1abd56d15d57d0f53
sajabenmussa-bit <sajabenmussa@gmail.com>
2025-12-10 15:23:27 +0200

    main class

---
+++ WorkPal java project/src/workpal/model/Project.java
@@ -0,0 +1,48 @@
+
+package workpal.model;
+
+import java.util.*;
+import javafx.concurrent.Task;
+
+
+public class Project {
+    private int projectId;
+    private int userId;
+    private String title;
+    private String description ;
+    private Date startDate;
+    private Date endDate;
+    private List<Task> tasks =new ArrayList<> ();
+    
+    
+    //Constructor
+    public Project(int projectId , int userId , String title, String description , Date startDate , Date endDate){
+        this.projectId=projectId;
+        this.userId= userId;
+        this.title= title;
+        this.description= description ;
+        this.startDate= startDate ;
+        this.endDate= endDate;     
+    }
+    //Methods
+    
+    public void addTask(Task t){
+        tasks.add(t);
+    }
+    
+   public void removeTask (Task t){
+       tasks.remove(t);
+   } 
+
+    public String getTitle() {
+        return title;
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    
+   
+  
+}
