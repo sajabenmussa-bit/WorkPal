4d5aeb20b01be632243182ea6b866e5ef5f3db6e
sajabenmussa-bit <sajabenmussa@gmail.com>
2025-12-11 21:24:21 +0200

    main class

--- WorkPal java project/src/workpal/model/WorkPal.java
+++ WorkPal java project/src/workpal/model/WorkPal.java
@@ -1,19 +1,24 @@
 
 package workpal.model;
+import workpal.ui.RegisterForm;
+import workpal.ui.ProjectForm;
 
 public class WorkPal {
     public static void main(String[] args) {
        
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+     javax.swing.SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-              
+                // إنشاء نموذج تسجيل المستخدم
                 RegisterForm registerForm = new RegisterForm();
                 registerForm.setVisible(true);
             }
         });
+      javax.swing.SwingUtilities.invokeLater(() -> {
+            new RegisterForm().setVisible(true);
+        });
+     
     
-    }
 }
 
 
