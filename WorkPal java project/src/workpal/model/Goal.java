d5c8cf0f0f16507bdb58e3630fc100b689f84479
sajabenmussa-bit <sajabenmussa@gmail.com>
2025-12-11 12:21:02 +0200

    main class

--- WorkPal java project/src/workpal/model/Goal.java
+++ WorkPal java project/src/workpal/model/Goal.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package workpal.model;
 
-
-
-import java.util.ArrayList;
-import java.util.Iterator;
+import java.util.*;
 
 public class Goal {
     private int goalId;
