348d7b27aaa3c55abf02f7d5dc733059d26166f0
sajabenmussa-bit <sajabenmussa@gmail.com>
2025-12-14 21:38:27 +0200

    Final edits

--- WorkPal/WorkPal java project/src/MainLauncher.java
+++ WorkPal/WorkPal java project/src/MainLauncher.java
@@ -1,16 +1,8 @@
 
 
-import Session.SessionManager;
+import workpal.Session.SessionManager;
 import workpal.ui.*;
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author Bashaer
- */
 public class MainLauncher {
     public static void main(String[] args){
         String loggedUser=SessionManager.getLoggedUser();
